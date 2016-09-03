b9be692c148f3289b34b922eac3550d7fed48610
Kristian Serrano <kristianserrano@gmail.com>
2016-09-03 00:12:19 -0400

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+
+    // 1. All configuration goes here
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        /*concat: {
+            // 2. Configuration for concatinating files goes here.
+        },*/
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'source/css/style.min.css': 'source/css/scss/style.scss',
+                    'source/css/pattern-scaffolding.min.css': 'source/css/pattern-scaffolding.scss'
+                }
+            }
+        },
+        shell: {
+            patternlab: {
+                //command: "php core/builder.php -gnc"
+                command: "php core/console --generate"
+            }
+        },
+        watch: {
+            options: {
+                livereload: true,
+            },
+            css: {
+                files: ['source/css/scss/**/*.scss', 'source/css/pattern-scaffolding.scss'],
+                tasks: ['sass','shell:patternlab'],
+                options: {
+                    spawn: true,
+                }
+            },
+            html: {
+                files: ['source/_patterns/**/*.mustache', 'source/_patterns/**/*.md', 'source/_patterns/**/*.json', 'source/_data/*.json'],
+                tasks: ['shell:patternlab'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    //grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-shell');
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['sass', 'shell:patternlab', 'watch']);
+};
---
+++ source/_patterns/atoms/30-buttons/00-buttons.md
@@ -0,0 +1,17 @@
+---
+title: Button Styles
+---
+These styles apply to states and context of the buttons (e.g. - default, primary, success, etc. as well as a disabled).
+
+<code>.btn</code> contains the base button styles. The following classes alter the button colors.
+
+<code>
+<pre>
+.btn-primary
+.btn-info
+.btn-success
+.btn-warning
+.btn-danger
+</pre>
+</code>
+The disabled button style is applied to any button that has the <code>disabled</code> boolean attribute.
