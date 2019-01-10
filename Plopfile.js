ada93604cedf00c3c38fc9b438886f61d85f5cf1
Lauren Hamel <lauren.hamel@outlook.com>
2019-01-10 11:28:51 -0500

    Enhances generators

--- Plopfile.js
+++ Plopfile.js
@@ -1,12 +1,25 @@
 module.exports = (plop) => {
 
   // Load dependencies.
-  const slugify = require('slugify');
   const fs = require('fs');
   const _ = require('lodash');
 
   // Configures generators.
   const config = {
+
+    // Define global configurations.
+    padding: 2,
+    regex: {
+      variation: () => /~(.+)$/,
+      order: () => new RegExp(`^\\d{${config.padding}}-`)
+    },
+    order: {
+      min: 00,
+      max: 99
+    },
+
+    // Define configurations for the pattern group generator.
+    group: {},
 
     // Define configurations for the pattern generator.
     pattern: {
@@ -22,6 +35,52 @@ module.exports = (plop) => {
     }
 
   };
+
+  // Extend lodash.
+  _.slugify = (text) => {
+
+    // Initialize helpers.
+    let variations;
+
+    // Extract any variations from the pattern string.
+    if( (variations = text.match(config.regex.variation())) ) {
+
+      variations = '~' + _.kebabCase(_.trimStart(variations[0], '~'));
+
+    } else { variations = ''; }
+
+    // Get the pattern slug.
+    let pattern = _.kebabCase(text.replace(config.regex.variation(), ''));
+
+    // Return the slug.
+    return `${pattern}${variations}`;
+
+  };
+  _.unslugify = (slug) => {
+
+    // Initialize helpers.
+    let variations;
+
+    // Remove any digits from the start of the slug.
+    slug = slug.replace(config.regex.order(), '');
+
+    // Wrap any variations at the end of the slug in parentheses.
+    if( (variations = slug.match(config.regex.variation())) ) {
+
+      variations = ' (' + _.startCase(_.trimStart(variations[0], '~')) + ')';
+
+    } else { variations = ''; }
+
+    // Get the title.
+    let title = _.startCase(slug.replace(config.regex.variation(), ''));
+
+    // Return the title.
+    return `${title}${variations}`;
+
+  };
+
+  // Define new helpers.
+  plop.setHelper('unslugify', (slug) => _.unslugify(slug));
 
   // Define an action type for creating new directories.
   plop.setActionType('mkdir', (answers, config, plop) => {
@@ -43,22 +102,52 @@ module.exports = (plop) => {
     prompts: [
       {
         type: 'input',
-        name: 'name',
-        message: 'Name of the pattern group',
-        filter: ( input ) => slugify(input)
+        name: 'pattern',
+        message: "What is the pattern group's name?",
+        filter: ( input ) => _.slugify(input)
       },
       {
         type: 'list',
         name: 'group',
-        message: "The pattern group's atomic group",
+        message: "What is the pattern group's atomic group?",
         choices: Object.keys(config.pattern.groups),
         default: Object.keys(config.pattern.groups)[0]
-      }
+      },
+      {
+        type: 'input',
+        name: 'order',
+        message: "What is the pattern group's order number? (optional)",
+        validate( input, answers ) {
+
+          // Allow non-answers.
+          if( _.isNil(input) ) return true;
+
+          // Otherwise, throw an error for values that fall outside the min and max order numbers.
+          if( +input < config.order.min || +input > config.order.max ) {
+
+            return `Please enter a number between ${config.order.min} - ${config.order.max}.`;
+
+          }
+
+          // Otherwise, assume the value is valid.
+          return true;
+
+        }
+      },
     ],
     actions(data) {
 
+      // Get the order prefix.
+      data.prefix = _.isNil(data.order) ? '' : _.padStart(data.order, config.padding, '0') + '-';
+
       // Extract atomic number.
-      const atomicNumber = _.padStart(config.pattern.groups[data.group], 2, '0');
+      data.atomic = _.padStart(config.pattern.groups[data.group], config.padding, '0');
+
+      // Determine the pattern group's name.
+      data.name = plop.renderString('{{prefix}}{{pattern}}', data);
+
+      // Determine the pattern group's path.
+      data.path = plop.renderString('src/_patterns/{{atomic}}-{{group}}/{{name}}', data);
 
       // Initialize actions.
       const actions = [];
@@ -66,14 +155,16 @@ module.exports = (plop) => {
       // 1. Create the new pattern group's folder.
       actions.push({
         type: 'mkdir',
-        path: `src/_patterns/${atomicNumber}-{{group}}/{{name}}`
+        path: data.path,
+        data
       });
 
       // 2. Create the new pattern group's MD file.
       actions.push({
         type: 'add',
-        path: `src/_patterns/${atomicNumber}-{{group}}/{{name}}.md`,
-        templateFile: 'templates/group/group.md'
+        path: '{{path}}.md',
+        templateFile: 'templates/group/group.md',
+        data
       });
 
       // Generate.
@@ -89,27 +180,79 @@ module.exports = (plop) => {
       {
         type: 'input',
         name: 'pattern',
-        message: 'Name of the pattern',
-        filter: ( input ) => slugify(input)
+        message: "What is the pattern's name?",
+        filter: ( input ) => _.slugify(input)
       },
       {
         type: 'list',
         name: 'group',
-        message: "The pattern's atomic group",
+        message: "What is the pattern's atomic group?",
         choices: Object.keys(config.pattern.groups),
         default: Object.keys(config.pattern.groups)[0]
       },
       {
+        type: 'list',
+        name: 'subgroup',
+        message: "Does the pattern belong to a pattern group?",
+        choices( answers ) {
+
+          // Initialize choices.
+          const choices = ['none'];
+
+          // Extract atomic number.
+          const atomic = _.padStart(config.pattern.groups[answers.group], config.padding, '0');
+
+          // Get the list of pattern groups, if any.
+          const groups = fs.readdirSync(`src/_patterns/${atomic}-${answers.group}`).filter((file) => fs.statSync(`src/_patterns/${atomic}-${answers.group}/${file}`).isDirectory());
+
+          // Return choices.
+          return choices.concat(groups);
+
+        },
+        default: 'No',
+        filter: ( input ) => input == 'none' ? false : input
+      },
+      {
+        type: 'input',
+        name: 'order',
+        message: "What is the pattern's order number? (optional)",
+        validate( input, answers ) {
+
+          // Allow non-answers.
+          if( _.isNil(input) ) return true;
+
+          // Otherwise, throw an error for values that fall outside the min and max order numbers.
+          if( +input < config.order.min || +input > config.order.max ) {
+
+            return `Please enter a number between ${config.order.min} - ${config.order.max}.`;
+
+          }
+
+          // Otherwise, assume the value is valid.
+          return true;
+
+        }
+      },
+      {
         type: 'confirm',
         name: 'js',
-        message: "Will this component use JS?",
+        message: "Will this pattern use JS?",
         default: false
       }
     ],
     actions(data) {
 
+      // Get the order prefix.
+      data.prefix = _.isNil(data.order) ? '' : _.padStart(data.order, config.padding, '0') + '-';
+
       // Extract atomic number.
-      const atomicNumber = _.padStart(config.pattern.groups[data.group], 2, '0');
+      data.atomic = _.padStart(config.pattern.groups[data.group], config.padding, '0');
+
+      // Determine the pattern's directory.
+      data.path = plop.renderString('src/_patterns/{{atomic}}-{{group}}' + (data.subgroup ? '/{{subgroup}}' : ''), data);
+
+      // Determine the pattern's name.
+      data.name = plop.renderString('{{prefix}}{{pattern}}', data);
 
       // Initialize actions.
       const actions = [];
@@ -117,36 +260,41 @@ module.exports = (plop) => {
       // 1. Create the new pattern's HBS file.
       actions.push({
         type: 'add',
-        path: `src/_patterns/${atomicNumber}-{{group}}/{{pattern}}.handlebars`,
-        templateFile: 'templates/pattern/pattern.handlebars'
+        path: '{{path}}/{{name}}.handlebars',
+        templateFile: 'templates/pattern/pattern.handlebars',
+        data
       });
 
       // 2. Create the new pattern's JSON file.
       actions.push({
         type: 'add',
-        path: `src/_patterns/${atomicNumber}-{{group}}/{{pattern}}.json`,
-        templateFile: 'templates/pattern/pattern.json'
+        path: '{{path}}/{{name}}.json',
+        templateFile: 'templates/pattern/pattern.json',
+        data
       });
 
       // 3. Create the new pattern's MD file.
       actions.push({
         type: 'add',
-        path: `src/_patterns/${atomicNumber}-{{group}}/{{pattern}}.md`,
-        templateFile: 'templates/pattern/pattern.md'
+        path: '{{path}}/{{name}}.md',
+        templateFile: 'templates/pattern/pattern.md',
+        data
       });
 
       // 4. Create the new pattern's SCSS file.
       actions.push({
         type: 'add',
-        path: `src/_patterns/${atomicNumber}-{{group}}/{{pattern}}.scss`,
-        templateFile: 'templates/pattern/pattern.scss'
+        path: '{{path}}/{{name}}.scss',
+        templateFile: 'templates/pattern/pattern.scss',
+        data
       });
 
       // 5. Create the new pattern's JS file.
       if( data.js ) actions.push({
         type: 'add',
-        path: `src/_patterns/${atomicNumber}-{{group}}/{{pattern}}.js`,
-        templateFile: 'templates/pattern/pattern.js'
+        path: '{{path}}/{{name}}.js',
+        templateFile: 'templates/pattern/pattern.js',
+        data
       });
 
       // Generate.
