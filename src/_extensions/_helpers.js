2345e48596c053667ebead00f9e3d4a8c2c89475
Lauren Hamel <lauren.hamel@outlook.com>
2019-01-24 11:28:12 -0500

    Updates handlebars extensions to load icons and logos via helpers

--- src/_extensions/_helpers.js
+++ src/_extensions/_helpers.js
@@ -1,5 +1,64 @@
 // Load helpers.
-const extend = require('extend');
+const _ = require('lodash');
+const glob = require('glob').sync;
+const fs = require('fs-extra');
+const path = require('path');
+
+// Load all icons and logos.
+const icons = glob('./src/icons/*.svg').reduce((result, icon) => {
+
+  // Get the icon's filename.
+  const name = path.basename(icon, path.extname(icon));
+
+  // Get the icon's contents.
+  const svg = fs.readFileSync(icon, 'utf8');
+
+  // Save the icons.
+  result[name] = svg;
+
+  // Continue.
+  return result;
+
+}, {});
+const logos = glob('./src/logos/*.svg').reduce((result, logo) => {
+
+  // Get the icon's filename.
+  const name = path.basename(logo, path.extname(logo));
+
+  // Get the icon's contents.
+  const svg = fs.readFileSync(logo, 'utf8');
+
+  // Save the icons.
+  result[name] = svg;
+
+  // Continue.
+  return result;
+
+}, {});
 
 // Export helpers.
-module.exports = extend(require('handlebars-helpers'), {});
+module.exports = _.extend(require('handlebars-helpers'), {
+
+  // Dynamically loads our icons by ID.
+  icon( id ) {
+
+    // Find our target icon file.
+    const icon = Object.keys(icons).filter((icon) => icon.indexOf(id) === 0)[0];
+
+    // Return the icon.
+    return icons[icon];
+
+  },
+
+  // Dynamically loads our logos by ID.
+  logo( id ) {
+
+    // Find our target logo file.
+    const logo = Object.keys(logos).filter((logo) => logo.indexOf(id) === 0)[0];
+
+    // Return the icon.
+    return logos[logo];
+
+  }
+
+});
--- src/_extensions/extensions.js
+++ src/_extensions/extensions.js
@@ -1,15 +1,26 @@
+// Load dependencies.
+const _  = require('lodash');
+
 // Load extensions.
 const helpers = require('./_helpers.js');
+const partials = require('./_partials.js');
 
 // Initialize extensions.
 module.exports = (engine) => {
 
   // Load helpers.
-  for( let key in helpers ) {
+  _.forIn(helpers, (helper, name) => {
 
-    if( helpers[key] ) engine.registerHelper(key, helpers[key]);
+    if( _.isFunction(helper) ) engine.registerHelper(name, helper);
 
-  }
+  });
+
+  // Load partials.
+  _.forIn(partials, (partial, name) => {
+
+    if( !_.isNil(partial) ) engine.registerPartial(name, partial);
+
+  });
 
   // Done.
   return engine;
