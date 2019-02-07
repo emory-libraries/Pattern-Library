b6ee4de9a6dfde85f3271d7dc07bee0fbb4e6ce7
Lauren Hamel <lauren.hamel@outlook.com>
2019-02-07 15:44:24 -0500

    Updates handlebars helpers

--- src/_extensions/helpers/string.js
+++ src/_extensions/helpers/string.js
@@ -1,6 +1,32 @@
+// Load dependencies.
+const _ = require('lodash');
+const regexEscape = require('escape-string-regexp');
+
 // Export helpers.
 module.exports = {
 
-  combine: ( ...strs ) => strs.slice(0, -1).join('')
+  combine( ...strs ) { return strs.slice(0, -1).join('') },
+
+  trimSubstringStart( str, substr, mods ) {
+
+    // Set mods if not set.
+    if( !_.isString(mods) ) mods = 'm';
+
+    // Trim.
+    return str.replace(new RegExp('^' + regexEscape(substr), mods), '');
+
+  },
+
+  trimSubstringEnd( str, substr, mods ) {
+
+    // Set mods if not set.
+    if( !_.isString(mods) ) mods = 'm';
+
+    // Trim.
+    return str.replace(new RegExp(regexEscape(substr) + '$', mods), '');
+
+  },
+
+  trimSubstring( str, substr ) { return this.trimSubstringEnd(this.trimSubstringStart(str, substr), substr); }
 
 };
