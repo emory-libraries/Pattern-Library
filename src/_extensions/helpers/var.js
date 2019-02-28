0f781bba69b2635c7e3775e98a7ff3dbcd647414
Lauren Hamel <lauren.hamel@outlook.com>
2019-02-28 12:04:16 -0500

    Adds `assign` and `unassign` variable helpers to handlebars

--- src/_extensions/helpers/var.js
+++ src/_extensions/helpers/var.js
@@ -4,30 +4,44 @@ const cast = require('../utils/cast.js');
 
 // Export helpers.
 module.exports = {
-  
+
   // Set a key-value pair on a given context.
   set( key, value, context ) {
-    
+
     context[key] = value;
-    
+
   },
-  
+
   // Unset a key-value pair on a given context.
   unset( key, context ) {
-    
+
     delete context[key];
-    
+
   },
-  
+
   // Make an object or array.
   make( key, context, options ) {
-    
+
     // Get the content.
     let content = options.fn(this).trim();
 
     // Create the object or array.
     context[key] = cast(content);
-    
+
+  },
+
+  // Assign a value to the current context.
+  assign( key, value ) {
+
+    _.set(this, key, value);
+
+  },
+
+  // Unassign a value to the current context.
+  unassign( key ) {
+
+    delete this[key];
+
   }
-  
+
 };
