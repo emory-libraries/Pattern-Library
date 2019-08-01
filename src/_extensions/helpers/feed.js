5c59989490fc093a1288fa7878b4e1a14e01d065
Lauren Hamel <lauren.hamel@emory.edu>
2019-08-01 17:00:53 -0400

    Adds custom `mapFeed` helper to allow inline mutations of fetched feed data

--- src/_extensions/helpers/feed.js
+++ src/_extensions/helpers/feed.js
@@ -57,6 +57,7 @@ const types = {
 // Export helpers.
 module.exports = {
 
+  // Fetch a feed from the given URL and return the feed's parsed content.
   fetchFeed( url ) {
 
     const response = request('GET', `${PROTOCOL}://cors-anywhere.herokuapp.com/${url}`, {
@@ -139,6 +140,109 @@ module.exports = {
     feed = removeXmlns(feed);
 
     // Return the parsed feed data.
+    return feed;
+
+  },
+
+  // Map all items within a feed to a given data model.
+  mapFeed( model, feed, options ) {
+
+    // Capture the context.
+    const context = this;
+
+    // Initialize a helper for binding source data within a value.
+    const bind = ( value, item, recursive = true ) => {
+
+      // Handle array and object values differently.
+      if( _.isArray(value) || _.isPlainObject(value) ) {
+
+        // Determine which map function to use.
+        const map = _.isArray(value) ? _.map : _.mapValues;
+
+        // Only bind things within the array or object if recursion is enabled.
+        if( recursive ) value = map(value, (v) => bind(v, item, recursive));
+
+      }
+
+      // Otherwise, handle simple values.
+      else {
+
+        // Initialize placeholders.
+        let placeholders;
+
+        // Search for placeholders that should be replaced with data from the given context.
+        if( (placeholders = value.match(/\{\:[\S]+\:\}/g)) ) {
+
+          // Bind the placeholder data into the value.
+          placeholders.forEach((placeholder) => {
+
+            // Capture the placeholder's key.
+            const key = placeholder.replace(/^\{\:|\:\}$/g, '');
+
+            // Bind data from the given context into the value.
+            value = value.replace(placeholder, _.get(context, key, ''));
+
+          });
+
+        }
+
+        // Search for placeholders that should be replaced with data from the feed item.
+        if( (placeholders = value.match(/\{[\S]+\}/g)) ) {
+
+          // Bind the placeholder data into the value.
+          placeholders.forEach((placeholder) => {
+
+            // Capture the placeholder's key.
+            const key = placeholder.replace(/^\{|\}$/g, '');
+
+            // Bind data from the given item's context into the value.
+            value = value.replace(placeholder, _.get(item, key, ''));
+
+          });
+
+        }
+
+      }
+
+      // Return the bound value.
+      return value;
+
+    };
+
+    // Map each item within the feed.
+    feed = feed.map((data) => {
+
+      // Loop through the data model, and made things as needed.
+      _.each(model, (value, key) => {
+
+        // Determine if the key is conditional.
+        if( _.endsWith(key, '?') ) {
+
+          // Get the key name without the conditional flag.
+          key = _.trimEnd(key, '?');
+
+          // Get the conditional that needs to be met in order for the value to be included.
+          const condition = bind(value.criteria, data);
+
+          // Get the criteria that must be met in order to display the conditional data.
+          const criteria = new Function(`return ${condition};`);
+
+          // Evaluate the criteria, and only include the value if the criteria was met.
+          if( criteria() ) data[key] = bind(value.value, data);
+
+        }
+
+        // Otherwise, bind the data as is.
+        else data[key] = bind(value, data);
+
+      });
+
+      // Return the updated data.
+      return data;
+
+    });
+
+    // Return the feed.
     return feed;
 
   }
