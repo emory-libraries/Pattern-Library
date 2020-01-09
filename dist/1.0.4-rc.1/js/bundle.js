fe8e6371ad342cd22df68596d5d5e5bf8b736c17
kristianserrano <kristianserrano@gmail.com>
2020-01-09 10:44:02 -0500

    Updates dist for 1.0.4-rc.1

--- dist/1.0.4-rc.1/js/bundle.js
+++ dist/1.0.4-rc.1/js/bundle.js
@@ -3052,8 +3052,9 @@ Components.register('calendar', {
         event.month = null;
         event.m = -1;
         event.year = -1; // Determine the month and year that the event falls in.
+        // If it's 0, change it to a string so that _.compact() keeps it or else it will drop.
 
-        event.m = _.compact(_.uniq([start.moment.isValid() ? start.moment.month() : null, end.moment.isValid() ? end.moment.month() : null]))[0];
+        event.m = _.compact(_.uniq([start.moment.isValid() ? start.moment.month().toString() : null, end.moment.isValid() ? end.moment.month().toString() : null]))[0];
         event.year = _.compact(_.uniq([start.moment.isValid() ? start.moment.year() : null, end.moment.isValid() ? end.moment.year() : null]))[0]; // Get the corresponding name of the month and year that the event falls in.
 
         event.month = moment.months(true)[event.m]; // Continue mapping events.
